0c507cd44da057bebb87c90276c37d7cf043dfa4
TATa <pf35301@gmail.com>
2016-05-30 00:20:56 +0900

    TimeLine取得追加

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-//post
+﻿//post
 //lib:CoreTweet
 using System;
 using System.Collections.Generic;
@@ -9,16 +9,13 @@ using System.Threading.Tasks;
 namespace post {
     class Program {
         static void Main(string[] args) {
-            Consumer matomeNaver = new Consumer("rLiy8r1ChLkjm58RmrJtsAuWg", "yby8MpKk95L2PCRm8hV6ZSYseYy6paPQRBl3PD28TpukgREVFo");
-            Token _Token = new Token("", "");
-            var tokens = new[] {
-                CoreTweet.Tokens.Create(matomeNaver.CK,matomeNaver.CS,_Token.token, _Token.tokenSecret)
-            };
-            
-            string text = "test";
-            foreach(var _tokens in tokens) {
-                _tokens.Statuses.Update(new { status = text});
-            }
+            Consumer _consumer = new Consumer("", "");
+            Token _token = new Token("", "");
+            Account account = new Account(_consumer, _token);
+
+            account.HomeTimeLine();
+            account.UserTimeLine();
+            account.tweet("test");
             Console.ReadKey();
         }
     }
@@ -27,19 +24,64 @@ namespace post {
         public string CK { get; private set; }
         public string CS { get; private set; }
 
-        public Consumer(string ck,string cs) {
-            CK = ck;
-            CS = cs;
+        public Consumer(string _ck,string _cs) {
+            CK = _ck;
+            CS = _cs;
         }
     }
 
     class Token {
-        public string token { get; private set; }
+        public string accessToken { get; private set; }
         public string tokenSecret { get; private set; }
 
-        public Token(string _token,string _tokenSecret) {
-            token = _token;
+        public Token(string _accessToken,string _tokenSecret) {
+            accessToken = _accessToken;
             tokenSecret = _tokenSecret;
+        }
+    }
+
+    class Account {
+        public string accessToken { get; private set; }
+        public string tokenSecret { get; private set; }
+        public string CK { get; private set; }
+        public string CS { get; private set; }
+        public string limitID;
+        CoreTweet.Tokens account;
+
+        public Account(Consumer _consumer,Token _token) {
+            accessToken = _token.accessToken;
+            tokenSecret = _token.tokenSecret;
+            CK = _consumer.CK;
+            CS = _consumer.CS;
+            account = CoreTweet.Tokens.Create(CK,CS,accessToken,tokenSecret);
+        }
+
+        public void tweet(string tweetText) {
+            account.Statuses.Update(new{status = tweetText});
+        }
+
+        public void HomeTimeLine() {
+            foreach(CoreTweet.Status status in account.Statuses.HomeTimeline(count => 5000)) {
+                //Console.WriteLine(status.GetType().Name);
+                Console.WriteLine("HomeTimeline");
+                if(status.User.ScreenName == limitID || limitID == null) {
+                    Console.WriteLine("User:" + status.User.ScreenName);
+                    Console.WriteLine(status.Text);
+                    Console.WriteLine("--------------------------------");
+                }
+            }
+        }
+
+        public void UserTimeLine() {
+            foreach(CoreTweet.Status status in account.Statuses.UserTimeline(count => 5000)) {
+                //Console.WriteLine(status.GetType().Name);
+                Console.WriteLine("UserTimeline");
+                if(status.User.ScreenName == limitID || limitID == null) {
+                    Console.WriteLine("User:" + status.User.ScreenName);
+                    Console.WriteLine(status.Text);
+                    Console.WriteLine("--------------------------------");
+                }
+            }
         }
     }
 }
