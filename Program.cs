24519e91e480320ae3caaefefe10c66e47b12338
TATa <pf35301@gmail.com>
2016-06-20 13:04:24 +0900

    Update Program.cs
    Streaming

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//post
+//post
 //lib:CoreTweet
 using System;
 using System.Collections.Generic;
@@ -6,107 +6,153 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using CoreTweet;
 using CoreTweet.Streaming;
 
-namespace post {
-    class Program {
-        static void Main(string[] args) {
-            Consumer _consumer = new Consumer("", "");
-            Token _token = new Token("", "");
+namespace tweet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Consumer _consumer = new Consumer("rLiy8r1ChLkjm58RmrJtsAuWg", "yby8MpKk95L2PCRm8hV6ZSYseYy6paPQRBl3PD28TpukgREVFo");
+            Token _token = new Token("3299527134-kMVohuPpsZ4IEO858OjCROT9LhtzdqFmi5MCd6P", "zs7vzSSHNOsdIxajpvYZSNMxtHxif5jpSrAaS3LeamTsi");
             Account account = new Account(_consumer, _token);
 
             //account.HomeTimeLine();
             //account.UserTimeLine();
             //account.follow("");
             //account.tweet(Console.ReadLine());
-            //account.userFav("",50);
+            //account.userFav("rinaty0514",100);
+            account.streamingFav("reizou05");
 
-            Console.WriteLine("------------終了-----------");
             Console.ReadKey();
         }
     }
 
-    class Consumer {
+    class Consumer
+    {
         public string CK { get; private set; }
         public string CS { get; private set; }
 
-        public Consumer(string _ck,string _cs) {
+        public Consumer(string _ck, string _cs)
+        {
             CK = _ck;
             CS = _cs;
         }
     }
 
-    class Token {
+    class Token
+    {
         public string accessToken { get; private set; }
         public string tokenSecret { get; private set; }
 
-        public Token(string _accessToken,string _tokenSecret) {
+        public Token(string _accessToken, string _tokenSecret)
+        {
             accessToken = _accessToken;
             tokenSecret = _tokenSecret;
         }
     }
 
-    class Account {
+    class Account
+    {
         public string accessToken { get; private set; }
         public string tokenSecret { get; private set; }
         public string CK { get; private set; }
         public string CS { get; private set; }
-        CoreTweet.Tokens account;
+        Tokens account;
 
-        public Account(Consumer _consumer,Token _token) {
+        public Account(Consumer _consumer, Token _token)
+        {
             accessToken = _token.accessToken;
             tokenSecret = _token.tokenSecret;
             CK = _consumer.CK;
             CS = _consumer.CS;
-            account = CoreTweet.Tokens.Create(CK,CS,accessToken,tokenSecret);
+            account = Tokens.Create(CK, CS, accessToken, tokenSecret);
         }
 
-        public void tweet(string tweetText) {
-            account.Statuses.Update(new{status = tweetText});
+        public void tweet(string tweetText)
+        {
+            account.Statuses.Update(new { status = tweetText });
+            Console.WriteLine("-----------------終了-----------------");
         }
 
-        public void HomeTimeLine() {
-            foreach(CoreTweet.Status status in account.Statuses.HomeTimeline(count => 5000)) {
+        public void HomeTimeLine()
+        {
+            Console.WriteLine("HomeTimeline");
+            foreach (CoreTweet.Status status in account.Statuses.HomeTimeline(count => 5000))
+            {
                 //Console.WriteLine(status.GetType().Name);
-                Console.WriteLine("HomeTimeline");
                 Console.WriteLine("User:" + status.User.ScreenName);
                 Console.WriteLine(status.Text);
                 Console.WriteLine("--------------------------------");
             }
+            Console.WriteLine("-----------------終了-----------------");
         }
 
-        public void UserTimeLine() {
-            foreach(CoreTweet.Status status in account.Statuses.UserTimeline(count => 5000)) {
+        public void UserTimeLine()
+        {
+            Console.WriteLine("UserTimeline");
+            foreach (CoreTweet.Status status in account.Statuses.UserTimeline(count => 5000))
+            {
                 //Console.WriteLine(status.GetType().Name);
-                Console.WriteLine("UserTimeline");
                 Console.WriteLine("User:" + status.User.ScreenName);
                 Console.WriteLine(status.Text);
                 Console.WriteLine("--------------------------------");
             }
+            Console.WriteLine("-----------------終了-----------------");
         }
 
-        public void follow(string target) {
-            try {
+        public void follow(string target)
+        {
+            try
+            {
                 account.Friendships.Create(screen_name => target);
                 Console.WriteLine($"{target}:Followed");
             }
-            catch {
+            catch
+            {
                 Console.WriteLine("Follow Failed");
             }
+            Console.WriteLine("-----------------終了-----------------");
         }
 
-        public void userFav(string userID,int _count) {
+        public void userFav(string userID, int _count)
+        {
             Console.WriteLine($"UserID:{userID}");
-            foreach(CoreTweet.Status status in account.Statuses.UserTimeline(screen_name => userID,count => _count)) {
-                try {
+            foreach (CoreTweet.Status status in account.Statuses.UserTimeline(screen_name => userID, count => _count))
+            {
+                try
+                {
                     account.Favorites.Create(id => status.Id);
                     Console.WriteLine(status.Text);
                     Console.WriteLine("---------------------------");
                 }
-                catch {
+                catch
+                {
                     Console.WriteLine("Fav Failed");
                 }
             }
+            Console.WriteLine("-----------------終了-----------------");
+        }
+
+        public void streamingFav(string userID)
+        {
+            Status st;
+
+            account.Streaming.UserAsObservable()
+                .Where((StreamingMessage m) => m.Type == MessageType.Create)
+                .Cast<StatusMessage>()
+                .Select((StatusMessage m) => m.Status)
+                .Subscribe((Status s) => Console.WriteLine($"User:{s.User.ScreenName}\n{s.Text}"));
+            
+
+            //Thread.Sleep(TimeSpan.FromSeconds(10));
+
+            //disposable.Dispose();
+
         }
     }
 }
